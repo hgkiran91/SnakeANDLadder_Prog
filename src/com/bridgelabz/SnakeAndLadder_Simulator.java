dd293fb6bc4674a13f2f86317fc30185d914c72b
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-04-22 18:33:30 +0530

    Added: UC1

---
+++ src/com/bridgelabz/SnakeAndLadder_Simulator.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder_Simulator {
+    public static void main(String[] args) {
+        int position = 0;
+        System.out.println("Initial Position: " + position);
+    }
+}
