f890314b730ccfbcd1513a4043ac2ec6d8731c23
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-04-22 18:41:18 +0530

    Added: UC2

--- src/com/bridgelabz/SnakeAndLadder_Simulator.java
+++ src/com/bridgelabz/SnakeAndLadder_Simulator.java
@@ -1,8 +1,19 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder_Simulator {
+
+    public static void getDieRoll() {
+        Random random = new Random();
+        int roll = random.nextInt(6) + 1;
+        System.out.println("Rolled Dice Number: " + roll);
+    }
+
     public static void main(String[] args) {
         int position = 0;
         System.out.println("Initial Position: " + position);
+
+        getDieRoll();
     }
 }
