1ac61c8a4c3c492c77c3bad05ddbbe5bd7e6119a
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-05-14 23:20:46 +0530

    Added: UC4_Repeating till the player reaches 100

--- src/com/bridgelabz/SnakeAndLadder_Simulator.java
+++ src/com/bridgelabz/SnakeAndLadder_Simulator.java
@@ -4,44 +4,37 @@ import java.util.Random;
 
 public class SnakeAndLadder_Simulator {
 
-    public static final int NO_PLAY = 1;
-    public static final int LADDER = 2;
-    public static final int SNAKE = 3;
-
-    public static int getDieRoll() {
-        Random random = new Random();
-        int roll = random.nextInt(6) + 1;
-        System.out.println("Rolled Dice Number: " + roll);
-        return roll;
-    }
-
-    public static int diceRollOption() {
-        Random random = new Random();
-        int optionCheck = random.nextInt(3) + 1;
-        System.out.println("Checking for option: " + optionCheck);
-        return optionCheck;
-    }
+    static final int NO_PLAY = 1;
+    static final int LADDER = 2;
+    static final int SNAKE = 3;
 
     public static void main(String[] args) {
         int position = 0;
-        System.out.println("Initial Position: " + position);
+        System.out.println("position: "+position);
+        while (position<100) {
+            Random ran = new Random();
+            int dice = ran.nextInt(6)+1;
+            System.out.println("dice: "+dice);
+            int optionCheck = ran.nextInt(3);
+            System.out.println("optionCheck: "+optionCheck);
 
-        int diceRoll = getDieRoll();
-        int option = diceRollOption();
 
-        switch (option) {
-            case NO_PLAY:
-                break;
-            case LADDER:
-                if (diceRoll > 0) {
-                    position = diceRoll + option;
-                }
-                break;
-            case SNAKE:
-                if (diceRoll >= 0) {
-                    position -= diceRoll;
-                }
+            if (optionCheck == LADDER) {
+                System.out.println("ladder!!!");
+                position = position + dice;
+            }
+            else if (optionCheck == SNAKE) {
+                System.out.println("snake!!!");
+                position = position - dice;
+            }
+            else {
+                System.out.println("noPlay!!!");
+                position = position;
+            }
+            if (position<0) {
+                position =0;
+            }
+            System.out.println("position: "+position);
         }
-        System.out.println("Current position:" + position);
     }
 }
